67f69f055601b070c33acdc47c6c365ae3837370
Keagan <esorts.k990@gmail.com>
2020-10-21 23:16:38 -0400

    training

---
+++ game/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Board from './components/Board';
+import Square from './components/Square';
+import Game from './components/Game';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+  <Game />
+    
+   
+    </div>
+  );
+}
+
+export default App;
